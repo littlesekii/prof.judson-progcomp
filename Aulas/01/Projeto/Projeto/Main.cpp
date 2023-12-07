b2450ba43feba9cc6e890aec9c31f75bc2109ae9
littlesekii <littlesekii@gmail.com>
2023-12-07 12:41:25 -0300

    Aula 1 e Lab 1 realizado.

---
+++ Aulas/01/Projeto/Projeto/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/* ERRADO */
+	/*
+	
+	cout << "Bem vindo ao C++";
+	coux << endl;
+	coup << "Meu primeiro programa!" << endl;
+
+	*/
+
+	/* CORRETO */
+	cout << "Bem vindo ao C++";
+	cout << endl;
+	cout << "Meu primeiro programa!" << endl;
+
+	return 0;
+}
