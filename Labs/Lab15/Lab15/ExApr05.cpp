30361e259e5191cb9d7d729b02f5da3052578706
littlesekii <littlesekii@gmail.com>
2024-01-03 17:50:57 -0300

    Lab 15 finalizado

--- Labs/Lab15/Lab15/ExApr05.cpp
+++ Labs/Lab15/Lab15/ExApr05.cpp
@@ -3,9 +3,44 @@
 
 using namespace std;
 
+struct ASCII {
+	char caractere;
+	int valor;
+};
+
+ASCII* alocaASCII(char, int);
+
 int main(void) {
 	system("chcp 1252 > nil");
 
+	// Declaração da variável
+	int numero;
+	char letra;
+
+	// Pedido de input
+	cout << "Insira uma letra: ";
+	cin >> letra;
+	// Pedido de input
+	cout << "Insira um número inteiro: ";
+	cin >> numero;
+
+	ASCII* ascii = alocaASCII(letra, numero);
+
+	cout << ascii << " - " << ascii->caractere << "|" << ascii->valor;
+
+	delete ascii;
 
 	return 0;
+}
+
+ASCII* alocaASCII(char caractere, int valor) {
+	// Aloca memória para o elemento ASCII
+	ASCII* ascii = new ASCII;
+
+	// Inicializa os campos do elemento ASCII
+	ascii->caractere = caractere;
+	ascii->valor = valor;
+
+	// Retorna o endereço de memória do elemento ASCII
+	return ascii;
 }
